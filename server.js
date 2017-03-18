fa60310db8778eb8c486481a0b5f944b8cc99fc9
netpi <moon@netpi.me>
2017-03-18 12:30:07 +0800

    add favicon

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const serve = (path, cache) => express.static(resolve(path), {
 })
 
 app.use(compression({ threshold: 0 }))
-// app.use(favicon('./public/logo-48.png'))
+app.use(favicon('./public/favicon.ico'))
 // app.use('/service-worker.js', serve('./dist/service-worker.js'))
 // app.use('/manifest.json', serve('./manifest.json'))
 app.use('/dist', serve('./dist'))
