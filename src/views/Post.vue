23cb331a2ab953d855853b9ba8960948a16db122
netpi <moon@netpi.me>
2017-05-30 22:34:11 +0800

    :stuck_out_tongue_winking_eye: add disqus for post page

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,33 +1,39 @@
 <template>
-    <div v-if="!loading" class ='wrap_posts'>  
-        <nav class="level">
-            <div class="level-item has-text-centered">
-                <div>
-                    <p class="title">{{post.title}}</p>
-                    <p class='desc'>
-                        <router-link :to="'/location/'+doubleBase64(formatLocation(post.location))" >
-                            <i class="fa fa-map-marker" aria-hidden="true"/>
-                            {{ post.location | formatLocation}}
-                        </router-link>
-                        <router-link :to="'/author/' + doubleBase64(post.authorname)">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        @{{ post.authorname}} ;
-                        </router-link>
-                        <i class="fa fa-clock-o" aria-hidden="true"></i>  
-                        {{ new Date(post.date)| formatDateTime}}
-                    </p>
+    <div>
+        <div v-if="!loading" class ='wrap_posts'>  
+            <nav class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="title">{{post.title}}</p>
+                        <p class='desc'>
+                            <router-link :to="'/location/'+doubleBase64(formatLocation(post.location))" >
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                {{ post.location | formatLocation}}
+                            </router-link>
+                            <router-link :to="'/author/' + doubleBase64(post.authorname)">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            @{{ post.authorname}} ;
+                            </router-link>
+                            <i class="fa fa-clock-o" aria-hidden="true"></i>  
+                            {{ new Date(post.date)| formatDateTime}}
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        <hr/>
-        <div class='content has-text-centered'>
-            <p>
-                {{post.content}}
-            <p/>
+            </nav>
+            <hr>
+            <div class='content has-text-centered'>
+                <p>
+                    {{post.content}}
+                </p>
 
-            <img v-for="url in post.images" :src="url | http2https" />
+                <img v-for="url in post.images" :src="url | http2https" />
+                <hr>
+                <div class='wrap_disqus'>
+                    <div  id="disqus_thread"></div>
+                </div>
+            </div>        
         </div>
-    </div>
+    </div>    
 </template>
 
 <script>
@@ -44,7 +50,7 @@ export default {
     mapState(['post', 'loading']),
     mapGetters(['location'])
   ),
-  created: function () {
+  mounted: function () {
     preFetch(this.$store)
   },
   preFetch: preFetch,
@@ -55,6 +61,19 @@ export default {
   methods: {
     doubleBase64,
     formatLocation
+  },
+  watch: {
+    loading (l) {
+      if (!l) {
+        (function () { // DON'T EDIT BELOW THIS LINE
+          let d = document
+          let s = d.createElement('script')
+          s.src = 'https://beauty-1.disqus.com/embed.js'
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s)
+        })()
+      }
+    }
   }
 }
 </script>
@@ -65,11 +84,15 @@ export default {
 .wrap_author {
     padding-left:10px
 }
+
 .desc {
     .fa {
     color: $gray;
     vertical-align: text-bottom;
     } 
 }
-
+.wrap_disqus {
+    width:80%;
+    margin:0 auto
+}
 </style>
